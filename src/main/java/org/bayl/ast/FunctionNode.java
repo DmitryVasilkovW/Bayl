c03d340dd7f144a208d9b7bfa85faaf10dd1574f
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-12-30 23:52:11 +0300

    refactor: codestyle

--- src/main/java/org/bayl/ast/FunctionNode.java
+++ src/main/java/org/bayl/ast/FunctionNode.java
@@ -1,24 +1,3 @@
-/*
- * Copyright (c) 2008 Cameron Zemek
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- */
 package org.bayl.ast;
 
 import java.util.ArrayList;
@@ -30,11 +9,6 @@ import org.bayl.runtime.Parameter;
 import org.bayl.runtime.UserFunction;
 import org.bayl.runtime.ZemObject;
 
-/**
- * Function declaration.
- *
- * @author <a href="mailto:grom@zeminvaders.net">Cameron Zemek</a>
- */
 public class FunctionNode extends Node {
     final static public List<Node> NO_PARAMETERS = new ArrayList<Node>(0);
 
@@ -61,7 +35,6 @@ public class FunctionNode extends Node {
                 parameterName = ((VariableNode) ((AssignNode) node).getLeft()).getName();
                 parameterValue = ((AssignNode) node).getRight().eval(interpreter);
             } else {
-                // This error should not occur
                 throw new RuntimeException("Invalid function");
             }
             Parameter param = new Parameter(parameterName, parameterValue);
