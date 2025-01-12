91ce21d9b435bd9827d02a2d2040fef3e0d7bbc8
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2025-01-13 01:51:19 +0300

    test: add tests for all operators

--- baylLanguage/src/test/java/util/TestData.java
+++ baylLanguage/src/test/java/util/TestData.java
@@ -4,7 +4,8 @@ import org.bayl.vm.executor.Executor;
 import java.util.List;
 
 public record TestData(
+        String message,
         List<String> bytecode,
-        Executor expected,
-        String message) {
+        Executor expected
+) {
 }
