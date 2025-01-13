a3c450e2f05776dc4ff4f3a217af719fa40d5883
dlavrenov <dlavrenov@ptsecurity.com>
2025-01-14 01:19:20 +0300

    feat: add division

--- baylLanguage/src/main/java/org/bayl/JNIExample.java
+++ baylLanguage/src/main/java/org/bayl/JNIExample.java
@@ -10,14 +10,16 @@ public class JNIExample {
     private native double generateMultiplicationTemplateDouble(double arg1, double arg2);
     private native int generateAdditionTemplateInt(int arg1, int arg2);
     private native double generateAdditionTemplateDouble(double arg1, double arg2);
+    private native double generateDivisionRemainderTemplateDouble(double arg1, double arg2);
+    private native double generateDivisionTemplateDouble(double arg1, double arg2);
 
     public static void main(String[] args) {
         JNIExample example = new JNIExample();
 
         int a = 5;
         int b = 3;
-        double aDouble = 1.5;
-        double bDouble = 3.0;
+        double aDouble = 3.0;
+        double bDouble = 1.4;
 
         int resultMultiplication = example.generateMultiplicationTemplateInt(a, b);
         System.out.println("Multiplication result: " + resultMultiplication);
@@ -30,5 +32,11 @@ public class JNIExample {
 
         double resultAdditionDouble = example.generateAdditionTemplateDouble(aDouble, bDouble);
         System.out.println("Addition result: " + resultAdditionDouble);
+
+        double resultDivisionRemainder = example.generateDivisionRemainderTemplateDouble(aDouble, bDouble);
+        System.out.println("Division remainder result: " + resultDivisionRemainder);
+
+        double resultDivision = example.generateDivisionTemplateDouble(aDouble, bDouble);
+        System.out.println("Division: " + resultDivision);
     }
 }
